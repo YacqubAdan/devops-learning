36602dc33be40240f547bbbadca856c25723fa3c
YacqubAdan <yacquubadan.1@gmail.com>
2024-12-20 23:42:08 +0000

    Project: DiskUtilisation = Complete

---
+++ bash/projects/DiskUtilisation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script that checks disk usage"
+
+echo "Check disk usage in Linux Environment"
+
+disk_size=$(df -h | grep /dev/sda3 | awk '{print $5}' | cut -d '%' -f1)
+
+echo "The disk size currently is: $disk_size$"
+
+if [$disk_size -gt 80 ];
+then
+	echo "Warning... disk size has more than 80% utilisation"
+else
+	echo "Enough Disk size is available"
+fi
+
+
