fefe5819beb1fdc499c5fecf598f72ff72c2a936
YacqubAdan <yacquubadan.1@gmail.com>
2024-12-20 23:46:19 +0000

    Project: DiskUtilisation = Complete

--- bash/projects/DiskUtilisation.sh
+++ bash/projects/DiskUtilisation.sh
@@ -6,9 +6,9 @@ echo "Check disk usage in Linux Environment"
 
 disk_size=$(df -h | grep /dev/sda3 | awk '{print $5}' | cut -d '%' -f1)
 
-echo "The disk size currently is: $disk_size$"
+echo "The disk size currently is: $disk_size%"
 
-if [$disk_size -gt 80 ];
+if [ $disk_size -gt 80 ];
 then
 	echo "Warning... disk size has more than 80% utilisation"
 else
